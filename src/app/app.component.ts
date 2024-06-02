2e71887d3d606a046bee3e48901f4d39adbe7a24
nabil el idrissi <nabil.el-idrissi@inetum.com>
2024-06-02 16:51:56 +0100

    v1 of portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FormationComponent } from './formation/formation.component';
+import { ServicesComponent } from './services/services.component';
+import { MyskilsComponent } from './myskils/myskils.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent,AboutComponent,AboutMeComponent,FormationComponent,ServicesComponent,MyskilsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/myskils/myskils.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myskils',
+  standalone: true,
+  imports: [],
+  templateUrl: './myskils.component.html',
+  styleUrl: './myskils.component.css'
+})
+export class MyskilsComponent {
+
+}
